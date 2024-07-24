cd26eb9ad3d4856abd23aa71b0b28a41bfc3d15e
AdrianEspino <79275395+AdrianEspino@users.noreply.github.com>
2024-07-24 09:23:47 +0200

    Instalador de librerias

--- Instalador.py
+++ Instalador.py
@@ -1,4 +1,22 @@
 import subprocess
 import sys
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "feedparser"])
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+packages = [
+    "requests",
+    "beautifulsoup4",
+    "tk",
+    "pillow",
+    "feedparser",
+    "csv",
+]
+
+for package in packages:
+    try:
+        install(package)
+    except Exception as e:
+        print(f"Error al instalar {package}: {e}")
+
+print("Instalacion de paquetes completa.")
