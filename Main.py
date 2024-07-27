d42339a22e4415754009b5f0fe136207fc42776b
AdrianEspino <adrian.espino@edu.uah.es>
2024-07-27 12:07:20 +0200

    Borrado

--- Main.py
+++
@@ -1,6 +0,0 @@
-from Interfaz import App
-from Funcionalidades import scrape_website, search_arxiv, search_pubmed
-
-if __name__ == "__main__":
-    app = App(scrape_website, search_arxiv, search_pubmed)
-    app.mainloop()
